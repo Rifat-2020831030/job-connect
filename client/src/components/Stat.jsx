fee96bce1d5717e3ab6b9c1c984b7cb889511c25
Ben <code.hasan@protonmail.com>
2025-08-05 18:19:25 +0600

    fit: last updated date of scraping

--- client/src/components/Stat.jsx
+++ client/src/components/Stat.jsx
@@ -7,7 +7,7 @@ const Stat = ({info, number, Icon}) => {
       <h3 className="text-3xl font-bold text-gray-900 mb-2">
         {number}
       </h3>
-      <p className="text-gray-600 font-medium">{info}</p>
+      <p className="text-gray-600 font-medium text-base sm:text-lg">{info}</p>
     </div>
   );
 };
